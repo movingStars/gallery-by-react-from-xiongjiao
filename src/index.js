f30924ce39fe60342822373c50f122d2706a435b
movingStars <1694056853@qq.com>
2017-06-04 16:31:32 +0800

    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
 
-
-
 // Render the main component into the dom
 ReactDOM.render(<App />, document.getElementById('app'));
